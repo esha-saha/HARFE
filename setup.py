126b360de4268abab15a8f585dc5b142df6ffdf6
esaha2703 <113554379+esaha2703@users.noreply.github.com>
2022-09-16 18:37:19 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import io
+
+VERSION = '0.0.1'
+
+def main():
+    with io.open("README.md", encoding="utf8") as f:
+        long_description = f.read().strip()
+
+    with open("requirements.txt") as f:
+            required = f.read().splitlines()
+
+    setup(
+        name='harfe',
+        url='https://github.com/njericha/srmdpy',
+        author='Esha Saha',
+        author_email='esaha@uwaterloo.ca',
+        packages=['harfe'],
+        install_requires=required,
+        version=VERSION,
+        license='MIT',
+        description='A Python implimentation of Hard Ridge Random Feature Expansion',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+    )
+
+if __name__ == '__main__':
+    main()
