9b004f8ee908c683d3c424efda9119bfabeab8cf
esaha2703 <113554379+esaha2703@users.noreply.github.com>
2022-09-16 18:18:40 +0000

    Update harfe.py

--- harfe/harfe.py
+++ harfe/harfe.py
@@ -11,9 +11,10 @@ import numpy as np
 from spgl1 import spg_bpdn
 import time
 
+from .data_simulation import generate_omega_bias, feature_matrix
 
-
-def harfe(y,c0 = None,s = None,tot_iter = None,thresh = None,mu = None,lam = None):
+def harfe(y, A, c0 = None,s = None,mu = None,lam = None,tot_iter = None,tol_error = None, tol_coeff = None) #, par1 = None,par2 = None,
+#                                        distribution = None,bool_bias = None, sparsity = None):
     
     """Implimentation of the Hard Ridge Random Feature Expansion algorithm
     **HARFE**
@@ -43,6 +44,9 @@ def harfe(y,c0 = None,s = None,tot_iter = None,thresh = None,mu = None,lam = Non
     y : numpy array of size (m,)
         True values of the function i.e., true iutput values.
         
+    A: matrix of size (N,d)
+        Random feature matrix of the form phi(Wx + b)
+        
     x : numpy array of size (m,d)
         The input data
         
@@ -68,10 +72,26 @@ def harfe(y,c0 = None,s = None,tot_iter = None,thresh = None,mu = None,lam = Non
         Initial vector c. If None is given, use c0 = np.zeros(N) as default
     mu : float, (default: None)
         Step size for first step of the algorithm. If None is given, default is mu = 0.1
-    thres : float, (default: None)
-        Threshold for convergence. If None is given, use thresh = 1e-3 as default
     lam: float, (default: None)
         Regularization parameter for the l2 min problem. If None os given, default is lam = 0.0001??????????
+    tot_iter: int, (default: None)
+        Maximum total iterations. If None is given, default is 200
+    tol_error : float, (default: None)
+        Threshold for convergence. If None is given, use thresh = 5e-3 as default
+    tol_coeff : float, (default: None)
+        Threshold for convergence. If None is given, use thresh = 1e-5 as default
+    weight: float, (default: None)
+        constant to be multiplied with the weights. If None is given, use weight = 1
+    par1: float, (default: None) 
+        First parameter for sampling the weights. If None is given, use par1 = -1
+    par2: float, (default: None)
+        Second parameter for sampling the weights. If None is given, use par1 = 1
+    distribution: str, (default: None)
+         Distribution for sampling the weighst and bias. If None is given, use distribution = 'nor-uni'
+    bias: bool, (default: None)
+         Option if bias is wanted or not. If None is given, use bias = True
+    sparsity: int, (default: None)
+         Number of nonzeros in each column of the weight matrix. If None is given, use sparsity  = 2
     
     verbosity : int, (default: 0)
         If 1 will print out feature info throughout the execution of the
@@ -82,6 +102,7 @@ def harfe(y,c0 = None,s = None,tot_iter = None,thresh = None,mu = None,lam = Non
     
     # Define useful constants
     m = len(y)       # Number of data points
+    d = int(x.shape[1])
 
     # Default parameter Handeling
     if N is None:
@@ -96,61 +117,64 @@ def harfe(y,c0 = None,s = None,tot_iter = None,thresh = None,mu = None,lam = Non
     if lam is None:
         lam = 0.0001
     
-    if thresh is None:
-        thresh = 1e-3
+    if tot_iter is None:
+        tot_iter = 200
+    
+    if tol_error is None:
+        tol_error = 5e-3
+    
+    if tol_coeff is None:
+        tol_coeff = 1e-5
+        
         
     if c0 is None:
         t = np.zeros(N)
     elif len(c0) != N:
         raise ValueError('Initial c0 should be of size N')
+        
+    
 
     
     error = np.zeros(tot_iter)
-    b = b.flatten()
+    y = b.flatten()
     C = np.zeros((A.shape[1],tot_iter))
     C[:,0] = c0
     i = 0
     iter_req = i
-    z1 = np.matmul(A.T,b)
+    z1 = np.matmul(A.T,y)
     z2 = np.matmul(A.T,A)
-    rel_err = np.linalg.norm(np.matmul(A,c0).flatten()-b)/np.linalg.norm(b)
+    rel_err = np.linalg.norm(np.matmul(A,c0).flatten()-y)/np.linalg.norm(y)
     start_time = time.time()
-    while rel_err>thresh:
+    while rel_err > 1e-10:
         c_tilde = C[:,i] + mu*(z1 - np.matmul(z2,C[:,i])) - ((mu*lam)*C[:,i])
         c_tilde_sort = np.sort(abs(c_tilde))
         idx = c_tilde >= c_tilde_sort[-s]
         A_pruned = A[:,idx]
-        z1_pruned = np.matmul(A_pruned.T,b)
+        z1_pruned = np.matmul(A_pruned.T,y)
         z2_pruned = np.matmul(A_pruned.T,A_pruned)
         c_pruned = np.matmul(np.linalg.pinv((z2_pruned + lam*np.identity(z2_pruned.shape[1])),rcond=1e-15)
                                  ,z1_pruned)
         c_pruned = c_pruned.flatten()
         erlst = 'nd'
         C[idx,i+1] = c_pruned
-        error[i+1] = np.linalg.norm(np.matmul(A,C[:,i+1]).flatten()-b)/np.linalg.norm(b)
+        error[i+1] = np.linalg.norm(np.matmul(A,C[:,i+1]).flatten()-y)/np.linalg.norm(y)
         iter_req = i+1
-#         print(np.count_nonzero(C[:,i+1]))
-#         error[i+1] = np.linalg.norm(C[:,i+1].flatten()-C[:,i].flatten())
-        print(np.linalg.norm(C[:,i+1].flatten()-C[:,i].flatten()),error[i+1])
-        if error[i+1]<=5e-3: #or np.linalg.norm(C[:,i+1].flatten()-C[:,i].flatten())<=1e-3:
-#             print(error[i+1],5e-3*np.linalg.norm(b))
-#             print(np.linalg.norm(C[:,i+1]))
+
+        if error[i+1]<=tol_error:
+
             break
-        elif np.linalg.norm(C[:,i+1].flatten()-C[:,i].flatten())<=1e-5:
+        elif np.linalg.norm(C[:,i+1].flatten()-C[:,i].flatten()) <= tol_coeff:
             break
+            
         rel_err = error[i+1]
-#         iter_req = i+1
-#         print('iter reqc-----------------------------------',iter_req)
         i = i+1
-#         iter_req = i
-#         print('iter req',iter_req)
+
         if i+1==tot_iter:
-#             print('Warning: You might want to use more iterations as maximum number of iterations of',tot_iter)
+
             iter_req = i
             break
                 
     end_time = time.time()  
-#     print("total time taken for HARFE ", end_time - start_time,'s')
-    print('Finally the total number of iterations the algorithm ran for was',iter_req+1)
+
     return C[:,iter_req],error[0:iter_req+1],iter_req+1,erlst,end_time - start_time
 
